d2aa4095966399f00f0b159778bbdd4e294f010d
Jack Cravener - 647364578 <jack.cravener@gm.com>
2015-05-17 11:40:48 -0400

    Got dad's clock working For some reason, passing the Adafruit_NeoPixel struct as a function parameter wasn't working properly so I had to make individual functions for each birthday...  I'll investigate during the making of the second clock.

--- wordclock.ino
+++ wordclock.ino
@@ -83,7 +83,17 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#define PIN_HAPPY 6
+#define PIN_HAPPY 2
+#define PIN_BIRTH 3
+#define PIN_DAY 4
+#define PIN_RICK 5
+#define PIN_SUSAN 6
+
+#define HAPPYLEN 5
+#define BIRTHLEN 5
+#define DAYLEN 3
+#define RICKLEN 4
+#define SUSANLEN 5
 
 // Parameter 1 = number of pixels in strip
 // Parameter 2 = Arduino pin number (most are valid)
@@ -92,12 +102,18 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel stripHappy = Adafruit_NeoPixel(4, PIN_HAPPY, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripHappy = Adafruit_NeoPixel(HAPPYLEN, PIN_HAPPY, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripBirth = Adafruit_NeoPixel(BIRTHLEN, PIN_BIRTH, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripDay = Adafruit_NeoPixel(DAYLEN, PIN_DAY, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripRick = Adafruit_NeoPixel(RICKLEN, PIN_RICK, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel stripSusan = Adafruit_NeoPixel(SUSANLEN, PIN_SUSAN, NEO_GRB + NEO_KHZ800);
 
 // IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
 // pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
 // and minimize distance between Arduino and first pixel.  Avoid connecting
 // on a live circuit...if you must, connect GND first.
+
+
 
 
 #include "Wire.h"
@@ -187,24 +203,19 @@ char Display1=0, Display2=0, Display3=0;
 
 // hardware constants
 
-int LEDClockPin=12;
+#define LEDClockPin 12
 
-int LEDDataPin=11;
+#define LEDDataPin 11
 
-int LEDLatchPin=8;
+#define LEDLatchPin 8
 
+#define PWMPin 9
 
+#define MinuteButtonPin 7
 
-int MinuteButtonPin=2;
+#define HourButtonPin 10
 
-int HourButtonPin=3;
-
-int BriteKnobPin=2; // analog read
-
-int PWMPin = 9;
-
-
-
+//int BriteKnobPin=2; // analog read
 
 
 void setup()
@@ -213,12 +224,20 @@ void setup()
   // neopixel setup
   stripHappy.begin();
   stripHappy.show(); // Initialize all pixels to 'off'
+  stripBirth.begin();
+  stripBirth.show(); // Initialize all pixels to 'off'
+  stripDay.begin();
+  stripDay.show(); // Initialize all pixels to 'off'
+  stripRick.begin();
+  stripRick.show(); // Initialize all pixels to 'off'
+  stripSusan.begin();
+  stripSusan.show(); // Initialize all pixels to 'off'
 
   // initialise the hardware	
   Wire.begin();
   // set the initial time here:
   // DS3231 seconds, minutes, hours, day, date, month, year
-  //setDS3231time(30,57,19,5,14,5,15);
+  //setDS3231time(30,17,17,7,16,5,15);
   
   // initialize the appropriate pins as outputs:
 
@@ -1181,6 +1200,102 @@ void incrementtime(void){
 }
 
 
+// rainbow for the happy birthday messages
+void rainbowCycleRick(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripHappy.setPixelColor(i, WheelRick(((i * 256 / stripHappy.numPixels()) + j) & 255));
+    }
+    stripHappy.show();
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripBirth.setPixelColor(i, WheelRick(((i * 256 / stripBirth.numPixels()) + j) & 255));
+    }
+    stripBirth.show();
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripDay.setPixelColor(i, WheelRick(((i * 256 / stripDay.numPixels()) + j) & 255));
+    }
+    stripDay.show();
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripRick.setPixelColor(i, WheelRick(((i * 256 / stripRick.numPixels()) + j) & 255));
+    }
+    stripRick.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t WheelRick(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return stripHappy.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return stripBirth.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return stripDay.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return stripRick.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return stripHappy.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return stripBirth.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return stripDay.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return stripRick.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return stripHappy.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return stripBirth.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return stripDay.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return stripRick.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
+
+void rainbowCycleSusan(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripHappy.setPixelColor(i, WheelRick(((i * 256 / stripHappy.numPixels()) + j) & 255));
+    }
+    stripHappy.show();
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripBirth.setPixelColor(i, WheelRick(((i * 256 / stripBirth.numPixels()) + j) & 255));
+    }
+    stripBirth.show();
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripDay.setPixelColor(i, WheelRick(((i * 256 / stripDay.numPixels()) + j) & 255));
+    }
+    stripDay.show();
+    for(i=0; i< stripHappy.numPixels(); i++) {
+      stripSusan.setPixelColor(i, WheelRick(((i * 256 / stripSusan.numPixels()) + j) & 255));
+    }
+    stripSusan.show();
+    delay(wait);
+  }
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t WheelSusan(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+   return stripHappy.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return stripBirth.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return stripDay.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+   return stripSusan.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else if(WheelPos < 170) {
+    WheelPos -= 85;
+   return stripHappy.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return stripBirth.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return stripDay.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+   return stripSusan.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return stripHappy.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return stripBirth.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return stripDay.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+   return stripSusan.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+}
 
 
 
@@ -1193,9 +1308,7 @@ void loop(void)
   /*analogWrite(PWMPin, analogRead(0)/4); //enable dimming via potentiometer or photoresistor*/
 
   analogWrite(PWMPin, 0); //manually set brightness level
-
-
-
+  
   //incrementtime();
 
   readDS3231time(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
@@ -1203,66 +1316,63 @@ void loop(void)
   displayTime();
   
   displaytime();
+  
+  if(dayOfMonth == 15 & month == 8)
+  {
+    rainbowCycleRick(10);
+  }
+  if(dayOfMonth == 22 & month == 9)
+  {
+    rainbowCycleSusan(10);
+  }
+
+    // test to see if the Minute Button is being held down
+
+    // for time setting
+
+    if ( (digitalRead(MinuteButtonPin) ==1 ) && second!=1) 
+
+      // the Minute Button is down and it has been more 
+
+      // than one second since we last looked
+
+    {
+
+      minute=(((minute/5)*5) +5)%60; 
+
+      second=0;
+
+      second++;  // Increment the second counter to ensure that the name
+
+      // flash doesnt happen when setting time
+      
+      setDS3231time(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
+      
+      displaytime();
+
+    }
 
 
 
-//    // test to see if the Minute Button is being held down
-//
-//    // for time setting
-//
-//    if ( (digitalRead(MinuteButtonPin) ==1 ) && second!=1) 
-//
-//      // the Minute Button is down and it has been more 
-//
-//      // than one second since we last looked
-//
-//    {
-//
-//      minute=(((minute/5)*5) +5); 
-//
-//      second=0;
-//
-//      //incrementtime();
-//      
-//      setDS3231time(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
-//
-//      second++;  // Increment the second counter to ensure that the name
-//
-//      // flash doesnt happen when setting time
-//
-//      displaytime();
-//
-//    }
-//
-//
-//
-//    // test to see if the Hour Button is being held down
-//
-//    // for time setting
-//
-//    if ((digitalRead(HourButtonPin)==1 ) && second!=1) 
-//
-//    {
-//
-//      minute = (minute/5)*5;  //round minute down to previous 5 min interval
-//
-//      if (++hour == 13) {
-//
-//        hour=1;  
-//
-//      }
-//
-//      //incrementtime();
-//      
-//      setDS3231time(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
-//
-//      second++;  // Increment the second counter to ensure that the name
-//
-//      // flash doesnt happen when setting time  
-//
-//      displaytime();
-//
-//    }
+    // test to see if the Hour Button is being held down
+
+    // for time setting
+
+    if ((digitalRead(HourButtonPin)==1 ) && second!=1) 
+
+    {
+
+      minute = (minute/5)*5;  //round minute down to previous 5 min interval
+
+      hour = (hour + 1) % 24;
+      second++;  // Increment the second counter to ensure that the name
+
+      // flash doesnt happen when setting time  
+      setDS3231time(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
+
+      displaytime();
+
+    }
 
 
 
